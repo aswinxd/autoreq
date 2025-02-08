f3d1d6a346379d14bfb7ca0c29d3816dc3eac9ed
private <155294690+aswinxd@users.noreply.github.com>
2025-02-08 22:30:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ, getenv
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+API_ID = int(environ.get("API_ID", "24428727"))
+API_HASH = environ.get("API_HASH", "1089a994258b8d77f06a2be5b1a01a31")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7946808913:AAGp_Coa3bnCMzVLoGml6xvQ-mr5gG90HI4")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002290791025"))
+ADMINS = int(environ.get("ADMINS", "1137799257"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://bot:bot@cluster0.8vepzds.mongodb.net/?retryWrites=true&w=mmajority")
+DB_NAME = environ.get("DB_NAME", "gfgfgfgt")
 
 # forcesub 
-FSUB = getenv("FSUB", "testkl17")
-CHID = int(getenv("CHID", ""))
+FSUB = getenv("FSUB", "@codecbots")
+CHID = int(getenv("CHID", "-1002068251462"))
