517fc973b45b6504ae2c9aa9df0b88c13163c5b4
private <155294690+aswinxd@users.noreply.github.com>
2025-02-08 22:52:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = environ.get("API_HASH", "1089a994258b8d77f06a2be5b1a01a31")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7946808913:AAGp_Coa3bnCMzVLoGml6xvQ-mr5gG90HI4")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002290791025"))
 ADMINS = int(environ.get("ADMINS", "1137799257"))
-DB_URI = environ.get("DB_URI", "mongodb+srv://bot:bot@cluster0.8vepzds.mongodb.net/?retryWrites=true&w=mmajority")
+DB_URI = environ.get("DB_URI", "mongodb+srv://bot:bot@cluster0.8vepzds.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "gfgfgfgt")
 
 # forcesub 
