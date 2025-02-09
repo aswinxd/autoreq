78063858a4af4c3cc16337d1d7ceefbfc86b6da7
private <155294690+aswinxd@users.noreply.github.com>
2025-02-09 20:11:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,13 +20,10 @@ class Bot(Client):
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
-            
-        print('Bot Started Powered By @VJ_Botz')
+        
 
 
     async def stop(self, *args):
-
         await super().stop()
-        print('Bot Stopped Bye')
 
 Bot().run()
